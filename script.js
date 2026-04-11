81db2e237863bd12b17c2432109c34b0e8f7b2d8
whoameeeee <160339507+kokosik2012R@users.noreply.github.com>
2026-04-11 18:47:19 +0300

    Add files via upload
    my first

---
+++ script.js
@@ -0,0 +1,36 @@
+alert("hello mir");
+let condition = true;
+
+function forward() {
+    anime({
+        targets: '.menu-small',
+        translateX: ['-100%', '0'],
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    });
+
+    condition = false
+}
+
+function backward() {
+    anime({
+        targets: '.menu-small',
+        translateX: ['0', '-100%'],
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    });
+
+    condition = true
+}
+
+$('.menu_small_icon').click(function () {
+    if (condition) {
+        forward();
+    } else {
+        backward();
+    }
+});
