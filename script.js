ae19563bb628046ba805e98bc5fe4a37c535f361
whoameeeee <160339507+kokosik2012R@users.noreply.github.com>
2026-04-12 16:11:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -35,4 +35,15 @@ $('.menu_small_icon').click(function () {
         backward();
     }
 });
-
+let links = document.querySelectorAll('.scroll');
+let targetID;
+links.forEach(function (element) {
+  element.addEventListener('click', function (event) {
+    event.preventDefault(); 
+    targetID = element.getAttribute('href'); 
+    document.querySelector(targetID).scrollIntoView({ 
+      behavior: 'smooth', 
+      block: 'start'
+    })
+  })
+})
